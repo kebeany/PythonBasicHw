f017755edd4daf9d4005941823ea70b06536c703
kebeany <138075261+kebeany@users.noreply.github.com>
2023-06-29 03:55:13 -0400

    Update main.py

--- 01/main.py
+++ 01/main.py
@@ -1,1 +1,62 @@
-
+# # # # print("hello")
+# # # # print("\"hello!\"said 'Johnny' brightly.")
+# # # # print('"hello!"said \'Johnny\' brightly.')
+# # # # print("monday tuesday wednesday thurday friday")
+# # # # print("monday","tuesday", "wednesday", "thursday", "friday")
+# # # # print("monday,tuesday,wednesday,thursday,friday")
+# # # # print("monday", "tuesday", "wednesday", "thursday", "friday", sep=", ")
+# # # # print(7+4) # 7+4=11
+# # # # print(17-2) # 17-2=15
+# # # # print(5*5) # 5*5=25
+# # # # print(10/2) # 10/2= 5
+# # # # print(3**3) # 3**3 = 27
+# # # # print(10//3) # 10//3=3
+# # # # print(37%10) # 37%10=7
+# # # # print(-13) # -13
+# # # # a = 80
+# # # # b = 75
+# # # # c = 65
+# # # # print((a + b + c) /3) # (a+b+c) /3 73.33333333333
+# # # print(1%2) #1
+# # # print(2%2) #0
+# # # print(3%2) #1
+# # # print(4%2) #0
+# # # print(2863%2) #1
+# # distance=12
+# # print(0.2467 * distance + 4.159) # 0.2467 * distance + 4.159= 7.1194
+# # print("hello todays weather is rainy")
+# # print('/a person walking by asked this/')
+# # print('/excuse me how do you get to the yoido subway station/')
+# # print('/the other person said/')
+# # # print('/I do not know /')
+# # a=14
+# # b=3
+# # print(a+b) # a+b=17
+# # print(a-b) # a-b=11
+# # print(a*b) # a*b=42
+# # print(a/b) # a/b=4.666666666666667
+# # print(a%b) # a%b=2
+# # a=263
+# # b=38
+# # print(a+b) # a+b=301
+# # print(a-b) # a-b=225
+# # print(a*b) # a*b=9994
+# # print(a/b) # a/b=6.921052631578948
+# # print(a%b) # a%b= 35
+# # a=10000
+# # b=25000
+# # c=30000
+# # print((a+b+c)/3) # (a+b+c)/3=21666,666666666668=d
+# price1 = 1000
+# price2 = 96900
+# print(price1-(price1*0.05))
+# print(price2-(price2*0.05))
+# f=50
+# print((f-32)/ 1.8) # ((f-32)/1.8)=10=c
+# a=2022
+# b=5
+# c=8
+# print((a+b)/c)
+# print((a/c)+(b/c))
+# print((a-b)/c)
+# print((a/c)-(b/c))
