9a3b9debd9d82e16be37149908ffb364448b927b
kebeany <138075261+kebeany@users.noreply.github.com>
2023-07-02 19:07:49 -0400

    Update main.py

--- 02/main.py
+++ 02/main.py
@@ -1,148 +1,148 @@
 
-#  age=13
-# print(age)
-# notebook=3
-# pencilcase=1
-# book=1
-# print(notebook,pencilcase,book)
-# print(13)
-# age=13
-# age=13
-# print(age)
-# print(age)
-# age=13
-# sentence="hello today is going to be warm"
-# print(sentence)
-# heads= 'todays weather is'
-# tails= ' very sunny'
-# print(heads+tails)
-# print( '=' * 50)
-# print ('my program')
-# print('=' * 50)
-# s1='python'
-# s2=' java '
-# s3= s1+''+ s2 + ''
-# print(s3 * 4)
-# a=3
-# b=4
-# print(a + b)
-# a = "3"
-# b = "4"
-# print(a + b)
-# hello="hello! I feel good today"
-# print(hello[5])
-# hello= "hello! I feel good today"
-# print(hello[-1])
-# hello="love yourself"
-# print(hello[0])
-# print(hello[2])
-# print(hello[7])
-# print(hello[-4])
-# print(hello[-1])
-# a= "my dogs name is choco"
-# print(a[11:13])
-# number="00avenue 2210"
-# print(number[-4:])
-# s1="good morning"
-# s2= s1[0:5] + s1[-1]
-# print(s2)
-# numberofapples= 3
-# destination='school'
-# print("I ate {0} apples".format (numberofapples,destination))
-# print("I ate {0} apples, and went to {1}".format(numberofapples,destination))
-# apple= 3
-# destination='school'
-# print("I ate {0} apples, and went to {1}".format (apple+1,destination))
-# apple = 3
-# destination ='school'
-# print(f'I ate {apple} apples, and I went to {destination}')
-# name= 'jack'
-# age=13
-# print(f'my name is {name}, and I am {age} years old')
-# age=13
-# print(f'my age is {age+1} years old')
-# name1='jack'
-# age1=10
-# name2='bob'
-# age2=13
-# print("my name:{0} my age: {1}" .format(name1,age1))
-# print("my name:{0} my age: {1}".format(name2,age2))
-# print(f'my name:{name1} my age: {age1}')
-# print(f'my name:{name2} my age: {age2}')
-# a = "hobby"
-# a.count('b')
-# a = "python is the best choice"
-# a.find('b')
-# a.find('k')
-# a = "life is too short"
-# a.index('t')
-# a.index('k')
-# ",".join('abcd')
-# a = "hi"
-# a.upper()
-# a = "HI"
-# a.lower()
-# a = "hi"
-# a.lstrip()
-# a = "hi"
-# a.rstrip()
-# a = "hi"
-# a.strip()
-# a = "life is too short"
-# a.replace("life","your leg")
-# a = "life is too short"
-# a.split()
-# b = "a:b:c:d"
-# b.split(':')
-# s = 'python study'
-# print(s[0])
-# print(s[2])
-# string = "how do I get to the yoido subway station?"
-# print(string[5])
-# print(string[-5])
-# print(string[10:12])
-# print(string[13:])
-# number = "00ga 1234"
-# print(number[4:])
-# str1 = 'whale coding'
-# str2 = "whale coding"
-# str3 = '' whale coding '' # this had trouble
-# str4 = "" whale coding "" # this had trouble
-# str5 = '''whale coding'''
-# str6 = """whale coding"""
-# str1 = "whale"
-# str2 = "coding"
-# print(str1 + str2)
-# print(str1*2)
-# print(str2 * 2)
-# print((str1 + str2 ) * 2)
-# s = "today's temperature is going to be {} degrees celsius"
-# 17,18,19,20
-# print(s.format(17))
-# print(s.format(18))
-# print(s.format(19))
-# print(s.format(20))
-# name = 'Python'
-# birthday_year = 2022
-# birthday_month = 12
-# birthday_day = 1
-# gender = 'boy'
-# print(name,birthday_year,birthday_month,birthday_day,gender)
-# s= 'python '
-# print(s * 10)
-# s = 'hello world'
-# print(s[-2])
-# print(s[-4])
-# print(s[-3])
-# print(s[-1])
-# phone_number = "010-1111-2222"
-# phone_number.replace( "-"," " )
-# starred problem
-# a = 472
-# b = 385
-# print(a)
-# print(b)
-# print(a*b)
-# print((b%10)*a)
-# print(((b//10)%10)* a)
-# print((b//100) * a)
+ age=13
+print(age)
+notebook=3
+pencilcase=1
+book=1
+print(notebook,pencilcase,book)
+print(13)
+age=13
+age=13
+print(age)
+print(age)
+age=13
+sentence="hello today is going to be warm"
+print(sentence)
+heads= 'todays weather is'
+tails= ' very sunny'
+print(heads+tails)
+print( '=' * 50)
+print ('my program')
+print('=' * 50)
+s1='python'
+s2=' java '
+s3= s1+''+ s2 + ''
+print(s3 * 4)
+a=3
+b=4
+print(a + b)
+a = "3"
+b = "4"
+print(a + b)
+hello="hello! I feel good today"
+print(hello[5])
+hello= "hello! I feel good today"
+print(hello[-1])
+hello="love yourself"
+print(hello[0])
+print(hello[2])
+print(hello[7])
+print(hello[-4])
+print(hello[-1])
+a= "my dogs name is choco"
+print(a[11:13])
+number="00avenue 2210"
+print(number[-4:])
+s1="good morning"
+s2= s1[0:5] + s1[-1]
+print(s2)
+numberofapples= 3
+destination='school'
+print("I ate {0} apples".format (numberofapples))
+print("I ate {0} apples, and went to {1}".format(numberofapples,destination))
+apple= 3
+destination='school'
+print("I ate {0} apples, and went to {1}".format (apple+1,destination))
+apple = 3
+destination ='school'
+print(f'I ate {apple} apples, and I went to {destination}')
+name= 'jack'
+age=13
+print(f'my name is {name}, and I am {age} years old')
+age=13
+print(f'my age is {age+1} years old')
+name1='jack'
+age1=10
+name2='bob'
+age2=13
+print("my name:{0} my age: {1}" .format(name1,age1))
+print("my name:{0} my age: {1}".format(name2,age2))
+print(f'my name:{name1} my age: {age1}')
+print(f'my name:{name2} my age: {age2}')
+a = "hobby"
+a.count('b')
+a = "python is the best choice"
+a.find('b')
+a.find('k')
+a = "life is too short"
+a.index('t')
+a.index('k')
+",".join('abcd')
+a = "hi"
+a.upper()
+a = "HI"
+a.lower()
+a = "hi"
+a.lstrip()
+a = "hi"
+a.rstrip()
+a = "hi"
+a.strip()
+a = "life is too short"
+a.replace("life","your leg")
+a = "life is too short"
+a.split()
+b = "a:b:c:d"
+b.split(':')
+s = 'python study'
+print(s[0])
+print(s[2])
+string = "how do I get to the yoido subway station?"
+print(string[5])
+print(string[-5])
+print(string[10:12])
+print(string[13:])
+number = "00ga 1234"
+print(number[4:])
+str1 = 'whale coding'
+str2 = "whale coding"
+str3 = '' whale coding '' # this had trouble
+str4 = "" whale coding "" # this had trouble
+str5 = '''whale coding'''
+str6 = """whale coding"""
+str1 = "whale"
+str2 = "coding"
+print(str1 + str2)
+print(str1*2)
+print(str2 * 2)
+print((str1 + str2 ) * 2)
+s = "today's temperature is going to be {} degrees celsius"
+17,18,19,20
+print(s.format(17))
+print(s.format(18))
+print(s.format(19))
+print(s.format(20))
+name = 'Python'
+birthday_year = 2022
+birthday_month = 12
+birthday_day = 1
+gender = 'boy'
+print(name,birthday_year,birthday_month,birthday_day,gender)
+s= 'python '
+print(s * 10)
+s = 'hello world'
+print(s[-2])
+print(s[-4])
+print(s[-3])
+print(s[-1])
+phone_number = "010-1111-2222"
+phone_number.replace( "-"," " )
+starred problem
+a = 472
+b = 385
+print(a)
+print(b)
+print(a*b)
+print((b%10)*a)
+print(((b//10)%10)* a)
+print((b//100) * a)
