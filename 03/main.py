bfdd23d1086d0444a0eff97c1b22bda1d1b4caac
kebeany <138075261+kebeany@users.noreply.github.com>
2023-07-03 18:59:25 -0400

    Create main.py

---
+++ 03/main.py
@@ -0,0 +1,162 @@
+name = input("what is your name?")
+print(name)
+age = input("how old are you?")
+print(age)
+name = input("what is your name?")
+age = int(input("what is your age?"))
+print('you are ' + name + ' and '+str(age) + ' years old ')
+print('you are ',name,'and ',(age) ,'years old ')
+print ('you are{}and{}years old.'.format(name, age))
+print(f'you are {name} and {age} years old ')
+print('please choose rock paper or scissors')
+Ide = input ()
+print('user chose:', Ide)
+Ide= input('rock paper or scissors ')
+print('the user picked:', Ide)
+date = input('todays date:')
+print("today's date is: ", date)
+date = input('todays date:')
+year = date[0:4]
+month= date[5:7]
+day= date[8:10]
+print('year:',year)
+print('month:',month)
+print('day:',day)
+def hello():
+    print("hello")
+def hello():
+    print ("hello")
+
+hello()
+hello()
+def hello():
+    print("hello")
+def message():
+    print("A")
+    print("B")
+
+
+message()
+print("C")
+message() 
+def say1(name):
+    string = "hello?"  + "Mr/Ms " + name 
+    return string
+def say2(name):
+        string = "hello?"  + "Mr/Ms " + name 
+        print(string)
+name = "jack"
+say1(name)
+say2(name)
+def say1(name):
+    string = "hello?"  + "Mr/Ms " + name 
+    return string
+def say2(name):
+        string = "hello?"  + "Mr/Ms " + name 
+        print(string)
+name = "jack"
+string = say1(name)
+print(string)
+def add (a,b):
+    return a + b
+def sub(a,b):
+    return a - b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a / b
+a = 6
+b = 3
+print(add(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(div(a,b))
+def add (a,b):
+    return a + b
+def sub(a,b):
+    return a - b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a / b
+a = 6
+b = 3
+print(add(b,a))
+print(sub(b,a))
+print(mul(b,a))
+print(div(b,a))
+def makingbrownie (butter,chocolate,nuts,vanilla,eggs):
+    print("put{}into a pot and melt the chocolate.".format(chocolate))
+    print("cut {} grams of butter into pan and melt the butter.".format(butter))
+    print("in a bowl mix {} eggs .".format(eggs))
+    print("in the bowl with the eggs, add melted chocolate and butter + {} grams of vanilla.".format(vanilla))
+    print("put the mixture into a deep pan and spread {} grams of nuts on top.".format(nuts))
+    print("bake an oven at 180 degrees celsuis for 25 to 30 minutes")
+    return 
+
+
+mybrownie = makingbrownie ("250", "2", "100", "50", "3")
+print(mybrownie)
+def sayhello():
+    print("hello,function!")
+print(sayhello)
+
+sayhello()
+print("say hello function once")
+sayhello()
+print('say hello function twice')
+sayhello()
+sayhello()
+print('say hello function five times')
+sayhello()
+sayhello()
+sayhello()
+sayhello()
+sayhello()
+print( 'hello function end.')
+    print("{}+{}={}".format(a,b,c))
+def plusten(a):
+    return a+10
+def add (a,b):
+    return a + b
+def sub(a,b):
+    return a - b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a / b
+a = 6
+b = 3
+a=100
+
+result = plusten(a)
+print(result)
+def smileyface(a):
+    return a + ':D'
+a = 'hello'
+def plusten(a):
+    return a+10
+
+result = smileyface(a)
+print(result)def add (a,b):
+def add(a,b):
+    return a + b
+def sub(a,b):
+    return a - b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a / b
+a = 10
+b = 5
+print(add(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(div(a,b))
+files
+
+addition(10,5)
+a = int('2023')
+b = float('36.5')
+c = str(2023)
+print(a, b, c)
