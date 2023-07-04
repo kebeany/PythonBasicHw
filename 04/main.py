bda85629f4272e6a86e357f9eda8928ba4aea2d3
kebeany <138075261+kebeany@users.noreply.github.com>
2023-07-04 18:57:16 -0400

    Update main.py

--- 04/main.py
+++ 04/main.py
@@ -1,1 +1,86 @@
 
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+print(not True)
+print(not False)
+print(10==10 and 10!=5)
+print(10>5 or 10<3)
+print(not 10>=3)
+print(not 10<10)
+number1 = 10
+number2 = 15
+number3 = 5
+string1 = 'good morning'
+string2 = 'hello'
+string3 = 'hi'
+bool1 = 10>2+5
+bool2 = 22>22+5
+print(number1)
+print(number2)
+print(number3)
+print(string1)
+print(string2)
+print(string3)
+print(bool1)
+# print(bool2)
+money=int(input("how much money do you have?"))
+if money>=100000 :
+    print("ride taxi")
+else:
+    print("walking")
+if False:
+    print("riding taxi")
+else:
+    print("walk")
+if False:
+    print("ride taxi")
+else:
+    print("walking")
+answer = input("did you win the game?(yes/no)")
+if answer == 'yes':
+    print("you can watch tv and rest.")
+else:
+    print("then do the dishes")
+x = 10 
+y = 8
+print(x > y)
+print(x < y)
+print(x == y)
+print(x != y)
+print(x >= y)
+print(x <= y)
+# print(x/10 < y)
+print(10 == 10 and 10 != 5)
+print(10 > 5 or 10 < 3)
+print(not 10 >= 5)
+print(not 10 < 10)
+print((3 == 3) or (4 != 3))
+print((3 == 3) and (4 != 3))
+
+print("1")
+if True :
+    print("2")
+else :
+    if False:
+        print("3")
+    else:
+        print("4")
+print("5")
+
+print("1")
+if True :
+    if False:
+        print("2")
+    else:
+        print("3")
+else :
+    print("4")
+print("5")
+
